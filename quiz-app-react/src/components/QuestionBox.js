32c46c236a1e80dbb631eba98bc06ffa8cc268e4
Piotr Polanski <polanski.pio@gmail.com>
2021-10-10 18:46:25 +0900

    added components

---
+++ quiz-app-react/src/components/QuestionBox.js
@@ -0,0 +1,22 @@
+import React, { userState } from 'react'
+
+const QuestionBox = ({ question, options }) => {
+  const [answer, setAnswer] = userState(options);
+  return (
+    <div className='questionBox'>
+      <div className='question'>{question}</div>
+      {answer.map((text, index) => (
+        <button
+          key={index}
+          className='answerBtn'
+          onClick={() => {
+            setAnswer([text])
+        }}>
+          {text}
+        </button>
+      ))}
+    </div>
+  )
+}
+
+export default QuestionBox;
